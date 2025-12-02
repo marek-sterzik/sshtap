7592e475b01b8702b7508ff4f6e4d2a1ed16c93b
Marek Sterzik <marek.sterzik@gmail.com>
2025-12-02 17:10:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# sshtap
+
+sshtap is a simple docker-based utility to build l2 tunnels using ssh connection.
+
+
+## scheme
+
+```
++-------------------+  ssh connection   +------------------+
+| client1 container | ----------------> |                  |
++-------------------+                   | server container |
++-------------------+  ssh connection   |                  |
+| client2 container | ----------------> |                  |
++-------------------+                   +------------------+
+       ...
+```
+
+In each of the container (server or client) a bridge br0 is created and bridges are L2-connected via the ssh connection.
+
+Both, server and client use the same docker image. If it will be run as a server or as a client will be decided according
+to environment variables.
+
+## environment variables
+
+### Server variables:
+
+    - `MODE` set to `server` to run in server mode
+    - `AUTHORIZED_KEYS` list of ssh keys authorized to connect to the server container
+
+### Client variables
+    - `MODE` set to `client` to run in client mode
+    - `PEER` set the peer to connect to in the form of `<host>[:<port>]`. If port is not given, default 22 is assumed
+    - `SSH_KEY` the ssh key to be used for connection to the peer
+    - `RESET_KNOWN_HOSTS` set to `yes` if you want to reset known hosts on startup, default `no`
+
+## persistent storage
+
+Persistent storage is used to keep:
+
+* permanent ssh host keys on server
+* known hosts on client
+
+While it is not necessary mandatory to use persistent storage, it is recommended to use it. Just mount the directory `/persistent` to any
+persistent storage and the given data will be kept.
---
+++ container/entrypoint.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+set -e
+
+run_server() {
+    mkdir -p /persistent/etc/ssh
+
+    ssh-keygen -A -f /persistent
+
+    if [ -n "$AUTHORIZED_KEYS" ]; then
+        echo "$AUTHORIZED_KEYS" > /root/.ssh/authorized_keys
+        chmod 0600 /root/.ssh/authorized_keys
+    else
+        echo "Warning: AUTHORIZED_KEYS variable not set, nobody is authorized" 1>&2
+        rm -f /root/.ssh/authorized_keys
+    fi
+
+    exec /usr/sbin/sshd -D
+}
+
+finish_client_config() {
+    ip link set dev tap0 master br0 up
+}
+
+parse_bool() {
+    if [ "$1" = "yes" -o "$1" = 1 -o "$1" = true ]; then
+        echo 1
+    elif [ "$1" = "no" -o "$1" = 0 -o "$1" = false ]; then
+        echo 0
+    elif [ "$1" = "" ]; then
+        echo "$2"
+    fi
+}
+
+run_client() {
+    if [ -z "$PEER" ]; then
+        echo "Error: missing PEER variable in client mode" 1>&2
+        return 1
+    fi
+    if [ -z "$SSH_KEY" ]; then
+        echo "Error: missing SSH_KEY variable in client mode" 1>&2
+        return 1
+    fi
+    RESET_KNOWN_HOSTS="`parse_bool "$RESET_KNOWN_HOSTS" "0"`"
+    if [ -z "$RESET_KNOWN_HOSTS" ]; then
+        echo "Error: invalid value for RESET_KNOWN_HOSTS, use either values 'yes' or 'no'" 1>&2
+        return 1
+    fi
+    if echo "$PEER" | grep -q ':[0-9]\+$'; then
+        export PORT="`echo "$PEER" | sed 's/^.*:\([0-9]\+\)$/\1/'`"
+        export PEER="`echo "$PEER" | sed 's/:[0-9]\+$//'`"
+    else
+        export PORT=22
+    fi
+
+    if [ "$RESET_KNOWN_HOSTS" = 1 ]; then
+        rm -f /persistent/known_hosts
+    fi
+    touch /persistent/known_hosts
+    rm -f /root/.ssh/known_hosts
+    ln -s /persistent/known_hosts /root/.ssh/known_hosts
+
+    touch /root/.ssh/key
+    chmod go-rwx /root/.ssh/key
+    echo "$SSH_KEY" > /root/.ssh/key
+    ssh-keygen -f /root/.ssh/key -y > /root/.ssh/key.pub
+    finished=""
+
+    sleep 2
+
+    ssh -i /root/.ssh/key -p "$PORT" -o "StrictHostKeyChecking=accept-new" -o "Tunnel=ethernet" -w 0:any root@"$PEER" 2>&1 | while read line; do
+        if [ -z "$finished" ] && echo "$line" | grep -q 'connected to server'; then
+            finish_client_config
+            finished=1
+        fi
+    done
+}
+
+ip link add name br0 type bridge
+ip link set dev br0 up
+
+if [ -z "$MODE" ]; then
+    if [ -z "$PEER" ]; then
+        export MODE="server"
+    else
+        export MODE="client"
+    fi
+fi
+
+if [ "$MODE" = "server" -o "$MODE" = "master" ]; then
+    run_server
+elif [ "$MODE" = "client" -o "$MODE" = "slave" ]; then
+    run_client
+else
+    echo "Error: invalid MODE: '$MODE'" 1>&2
+    exit 1
+fi
